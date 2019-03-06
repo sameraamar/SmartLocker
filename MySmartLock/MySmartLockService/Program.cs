1934d957cd911c4d8ba47660bfcb5dd07f051d68
sameraamar <samer.aamar@gmail.com>
2019-03-06 12:39:13 +0200

    initial version

---
+++ MySmartLock/MySmartLockService/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MySmartLockWatchDog
+{
+    using System.ServiceProcess;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new TestService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ MySmartLock/MySmartLockUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySmartLockUI
+{
+    static class Program
+    {
+        private static Mutex mutex = null;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            const string appName = "MyAppName";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //MessageBox.Show(appName + " is already running! Exiting the application.");
+                return;
+            }
+
+            Application.Run(new Form1());
+        }
+    }
+}
