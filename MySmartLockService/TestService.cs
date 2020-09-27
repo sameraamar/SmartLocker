8cb90c159e509d226c44f92e5b6e77d976a3e5f8
Samer Aamar <saaamar@microsoft.com>
2020-09-27 08:04:23 +0300

    usings

--- MySmartLockService/TestService.cs
+++ MySmartLockService/TestService.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.ServiceProcess;
-using System.Text;
 
 namespace MySmartLockWatchDog
 {
